4d7b8949aac3c44b25d32f3dcf2a1ca736714f53
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2023-05-21 01:00:13 +0200

    test

--- ipv6_test.py
+++ ipv6_test.py
@@ -4,7 +4,7 @@ def test_expand_positive():
     assert ipv6_expand('::BBBB:2') == '0000:0000:0000:0000:0000:0000:BBBB:0002'
 
 def test_expand_negative():
-    assert ipv6_expand('::BBBB:2') != '::BBBB:2'
+    assert ipv6_expand('::BBBB:2') == '::BBBB:2'
 
 def test_expand_not_empty_string():
     assert ipv6_expand('::BBBB:2') != ''
